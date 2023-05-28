fcb73b1baf4c1b42f3634a0fc48183ac3e432e9a
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-05-28 13:38:34 +0330

    Implement submission post and list api

--- challenge/models.py
+++ challenge/models.py
@@ -21,4 +21,6 @@ class GroupParticipation(models.Model):
 class GroupSubmit(models.Model):
     phase = models.ForeignKey(ChallengePhase, on_delete=models.CASCADE)
     group = models.ForeignKey(ChallengeGroup, on_delete=models.CASCADE)
-    score = models.DecimalField(max_digits=50, decimal_places=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField()
+    score = models.DecimalField(max_digits=50, decimal_places=20, null=True, blank=True)
--- challenge/views.py
+++ challenge/views.py
@@ -1,22 +1,27 @@
+from functools import cached_property
+
 from django.conf import settings
 from django.db.models import Max
 from django.http import Http404
-from django.shortcuts import render
-from rest_framework import generics, mixins, status
+from rest_framework import generics, mixins, status, serializers
+from rest_framework.generics import ListCreateAPIView, get_object_or_404
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from challenge.models import Challenge, ChallengePhase, GroupParticipation, GroupSubmit
-from challenge.serializers import ChallengeSerializer, ChallengePhaseSerializer, GroupParticipationSerializer, \
-    GroupSubmitSerializer
+from challenge.models import Challenge, ChallengePhase, GroupSubmit
+from challenge.serializers import (
+    ChallengeSerializer, ChallengePhaseSerializer, GroupSubmitSerializer,
+)
 from groups.models import ChallengeGroup
 
 
 # Create your views here.
 
-class ChallengeDetailAPIView(mixins.RetrieveModelMixin,
-                             generics.GenericAPIView):
+class ChallengeDetailAPIView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
 
@@ -24,8 +29,10 @@ class ChallengeDetailAPIView(mixins.RetrieveModelMixin,
         return self.retrieve(request, *args, **kwargs)
 
 
-class ChallengeListAPIView(mixins.ListModelMixin,
-                           generics.GenericAPIView):
+class ChallengeListAPIView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView
+):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
 
@@ -33,13 +40,50 @@ class ChallengeListAPIView(mixins.ListModelMixin,
         return self.list(request, *args, **kwargs)
 
 
-class ChallengePhaseAPIView(mixins.RetrieveModelMixin,
-                            generics.GenericAPIView):
+class ChallengePhaseAPIView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
     queryset = ChallengePhase.objects.all()
     serializer_class = ChallengePhaseSerializer
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+
+
+class ListCreateGroupSubmitAPIView(ListCreateAPIView):
+    class GroupSubmitSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = GroupSubmit
+            fields = [
+                "file",
+                "score",
+                "created_at",
+            ]
+            extra_kwargs = {
+                "file": {"write_only": True},
+                "score": {"read_only": True},
+                "created_at": {"read_only": True},
+            }
+
+    serializer_class = GroupSubmitSerializer
+
+    @cached_property
+    def phase(self):
+        return get_object_or_404(
+            ChallengePhase,
+            name=self.kwargs["phase_name"],
+            challenge__name=self.kwargs["challenge_name"],
+        )
+
+    def get_queryset(self):
+        return GroupSubmit.objects.filter(
+            group_id=self.request.user.challenge_user.group_id,
+            phase=self.phase
+        ).order_by("-id")
+
+    def perform_create(self, serializer):
+        serializer.save(phase=self.phase, group_id=self.request.user.challenge_user.group_id)
 
 
 class GroupSubmitAPIView(APIView):
@@ -78,5 +122,6 @@ class ScoreboardAPIView(APIView):
         except ChallengePhase.DoesNotExist:
             raise Http404
         submits = GroupSubmit.objects.filter(phase=phase).values('group').annotate(best_score=Max('score')).order_by(
-            '-best_score')
+            '-best_score'
+        )
         return Response(GroupSubmitSerializer(submits, many=True), status.HTTP_200_OK)
