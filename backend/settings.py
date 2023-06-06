e20b59ca9000fdf635d51f5ab6283d5002e2a64e
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-06-07 00:20:53 +0330

    Fix db

--- backend/settings.py
+++ backend/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': os.environ.get('DB_NAME'),
+            'PASSWORD': os.environ.get('DB_PASS'),
+            'HOST': 'db-postgres',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
