b6c41c69101970b8000153350e3b8a7d38c855de
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-06-07 01:17:10 +0330

    Don't send .zip in rmq

--- challenge/views.py
+++ challenge/views.py
@@ -104,7 +104,7 @@ class ListCreateGroupSubmitAPIView(ListCreateAPIView):
                         'tag': self.phase.tag,
                         'student_number': self.request.user.challenge_user.student_code,
                         'file_id': submit.id,
-                        'file_name': submit.file.name,
+                        'file_name': submit.file.name.rsplit(".", 1)[0],
                     }
                 ).encode()
             )
