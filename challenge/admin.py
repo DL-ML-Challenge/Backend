7f07ca4ffd2471b2dba6359850be382830d9567f
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-06-23 20:11:12 +0330

    Add admin for ml scoring

--- challenge/admin.py
+++ challenge/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from challenge.models import GroupSubmit
+
+
+class MLSubmission(GroupSubmit):
+    class Meta:
+        proxy = True
+        verbose_name = "ML Submissions"
+
+
+@admin.register(MLSubmission)
+class MLSubmissionAdmin(admin.ModelAdmin):
+    fields = ["group", "file", "score", "team_members"]
+    readonly_fields = ["group", "file", "team_members"]
+    list_display = ["id", "group", "score"]
+    actions_on_top = []
+
+    def get_queryset(self, request):
+        return MLSubmission.objects.all().filter(phase__challenge__name="ml")
+
+    @admin.display(description="Team Members")
+    def team_members(self, instance: MLSubmission):
+        return "\n".join(instance.group.challengeuser_set.values_list("full_name", flat=True))
